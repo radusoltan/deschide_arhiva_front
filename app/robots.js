a2f760e359e3c3816e9b60368f61693d1689dd61
Radu Soltan <radusoltan@gmail.com>
2024-10-17 19:55:06 +0300

    updated 14;

---
+++ app/robots.js
@@ -0,0 +1,13 @@
+const robots = ()=>{
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: process.env.NEXT_PUBLIC_APP_URL+'sitemap.xml',
+  }
+
+}
+
+export default robots
